16c773376602db32a78e65ee06e60f8dfa789633
realfakenerd <lucas210898@hotmail.com>
2023-10-18 18:00:42 -0300

    added more commands

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,27 @@
 #! /usr/bin/env node
-import { bold, green } from 'colorette';
-import { program } from 'commander';
-import _sharp from 'sharp';
+import { bgGreen, bgGreenBright, bold, green } from "colorette";
+import { program } from "commander";
+import _sharp from "sharp";
 
-const sizes = [100, 200, 400, 800, 1080];
+// const sizes = [100, 200, 400, 800, 1080];
 
-program.name('sharp').description('CLI commands to sharpen you vision ;)').version('0.0.1')
+program
+    .name(`${bgGreenBright(' ') + bgGreen(' ')}${bold(bgGreen("sharp "))}`)
+    .description("CLI commands to sharpen you vision ;)")
+    .version("0.0.1");
 
-program.command('input')
-
-program.parse();
-const input = program.args[0];
-const output = program.args[1];
+program
+    .command(`resize`)
+    .alias("resise")
+    .description(green("Resize image to width, height or width x height"))
+    .argument("source", green("Source file path"))
+    .argument("[desination]", )
+    .option('-w, --width <number>', 'New width', '1920')
+    .option('-h, --height <number>', 'New height', '1080');
 
 console.log(program.args);
+program.parse();
+
 // /** @type {import('sharp').Sharp} */
 // let sharp;
 
@@ -25,13 +33,11 @@ console.log(program.args);
 //     sharp = _sharp(input).toFormat('webp', { quality: 80 });
 // }
 
-
 // await Promise.all(sizes.map(async (size) => {
 //     try {
 //         resizeImage(size)
 //     } catch {/* empty */}
 // }))
-
 
 // /**
 //  * The function `resizeImage` resizes an image to a specified height and saves it as a webp file.
