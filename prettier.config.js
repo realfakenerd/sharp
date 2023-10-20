4adfd11792d3c291a819f42424d188d82030c430
realfakenerd <lucas210898@hotmail.com>
2023-10-20 17:26:53 -0300

    ci(tools): added prettier and refactored rollug config

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Options} */
+export default {
+	useTabs: true,
+	trailingComma: 'none',
+	printWidth: 100,
+	semi: true,
+	singleQuote: true
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,25 @@
-import terser from "@rollup/plugin-terser";
-import alias from "@rollup/plugin-alias";
+import alias from '@rollup/plugin-alias';
 
 /** @type {import('rollup').RollupOptions} */
 export default {
-  input: "dist/index.js",
-  plugins: [
-    alias({
-      entries: [{ find: "$utils", replacement: "dist/utils/index.js" }],
-    }),
-    // terser({
-    //   compress: {
-    //     ecma: 2020,
-    //     passes: 3,
-    //     module: true,
-    //   },
-    // }),
-  ],
-  output: {
-    inlineDynamicImports: true,
-    file: "bin/index.js",
-    format: "es",
-  },
-  external: ["colorette", "figlet", "sharp", "@commander-js/extra-typings"],
-  cache: true,
+	input: 'dist/index.js',
+	plugins: [
+		alias({
+			entries: [{ find: '$utils', replacement: 'dist/utils/index.js' }]
+		})
+		// terser({
+		//   compress: {
+		//     ecma: 2020,
+		//     passes: 3,
+		//     module: true,
+		//   },
+		// }),
+	],
+	output: {
+		inlineDynamicImports: true,
+		file: 'bin/index.js',
+		format: 'es'
+	},
+	external: ['colorette', 'nanospinner', 'figlet', 'sharp', '@commander-js/extra-typings'],
+	cache: true
 };
